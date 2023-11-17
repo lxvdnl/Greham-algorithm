d48e303f86a9411a87073d3f94d3aa5a7a18a55c
Danila <alexseev_dnl@mail.ru>
2023-11-18 00:52:32 +0300

    small fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "../headers/AVL_Tree.h"
 #include "../headers/Point.h"
 #include "../headers/QuickSort.h"
@@ -12,44 +10,27 @@ int main()
 	std::cout << "Enter 1 - to use quick sort, 2 - to use avl tree sort: ";
 	int choice;
 	std::cin >> choice;
+	if (choice != 1 && choice != 2)
+		return 0;
 	std::cout << "Enter the number of points: ";
 	std::cin >> pointsCount;
-	std::cout << "Enter n dots separated by spaces\n";
+	std::cout << "Enter " << pointsCount << " dots separated by spaces\n";
+	if (pointsCount < 3) {
+		pointsInputExceptions(pointsCount);
+		return 0;
+	}
+	point* pointsField = new point[pointsCount];
+	pointsFieldCompletion(pointsField, pointsCount);
+	lexicographicallyMinimalPoint(pointsField, pointsCount);
+	transferCoordinateSystem(pointsField, pointsCount);
 	if (choice == 1)
-	{
-		if (pointsCount < 3) {
-			pointsInputExceptions(pointsCount);
-			return 0;
-		}
-		point* pointsField = new point[pointsCount];
-		pointsFieldCompletion(pointsField, pointsCount);
-		lexicographicallyMinimalPoint(pointsField, pointsCount);
-		transferCoordinateSystem(pointsField, pointsCount);
 		quickSort(pointsField, 1, pointsCount - 1);
-		stack st(pointsCount);
-		determinePointsFormingMaxConvexHull(pointsField, pointsCount, st);
-		st.reverse();
-		printMaxConvexHull(st);
-		
-		delete[] pointsField;
-	}
-	else if (choice == 2)
-	{
-		if (pointsCount < 3) {
-			pointsInputExceptions(pointsCount);
-			return 0;
-		}
-		point* pointsField = new point[pointsCount];
-		pointsFieldCompletion(pointsField, pointsCount);
-		lexicographicallyMinimalPoint(pointsField, pointsCount);
-		transferCoordinateSystem(pointsField, pointsCount);
+	else
 		treeSort(pointsField, pointsCount);
-		stack st(pointsCount);
-		determinePointsFormingMaxConvexHull(pointsField, pointsCount, st);
-		st.reverse();
-		printMaxConvexHull(st);
-		
-		delete[] pointsField;
-	}
+	stack st(pointsCount);
+	determinePointsFormingMaxConvexHull(pointsField, pointsCount, st);
+	st.reverse();
+	printMaxConvexHull(st);
+	delete[] pointsField;
 	return 0;
 }
